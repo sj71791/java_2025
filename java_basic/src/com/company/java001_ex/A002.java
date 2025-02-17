08af9a29f5f54393995aa04bba332a74ea39c5ec
sj <sj71791@naver.com>
2025-02-17 17:57:49 +0900

    2025-02-17 java001

---
+++ java_basic/src/com/company/java001_ex/A002.java
@@ -0,0 +1,8 @@
+package com.company.java001_ex;
+
+public class A002 {
+	public static void main(String [] args) {
+		System.out.println("내이름은 이수정입니다");
+	}
+
+}
---
+++ java_basic/src/com/company/java001_ex/A003.java
@@ -0,0 +1,8 @@
+package com.company.java001_ex;
+
+public class A003 {
+	public static void main(String [] args) {
+		System.out.println("A\nAB\nABC"); // \n 줄바꿈
+	}
+
+}
