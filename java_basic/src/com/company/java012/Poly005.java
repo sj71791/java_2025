39cbd1f7a93ec61d373cbf286f23df39c89cbc52
jaewon <wodnjs0610@naver.com>
2025-03-05 18:17:21 +0900

    2025-03-05 java012

--- java_basic/src/com/company/java012/Poly005.java
+++ java_basic/src/com/company/java012/Poly005.java
@@ -1,10 +1,11 @@
 package com.company.java012;
 /*
-		  Object
-		    ↑
+		   Object
+		     ↑
 		   Calc (exec)
-	↑	  ↑		↑		↑
+	 ↑	   ↑	↑		↑
 	plus Minus Multi  Divide
+	(exec)(exec)(exec)(exec)
  */
 class Calc extends Object{void exec(double d1, double d2){System.out.println();}}
 class Plus extends Calc{void exec(double d1, double d2){System.out.println(d1+d2);}}
@@ -14,10 +15,11 @@ class Divide extends Calc{void exec(double d1, double d2){System.out.println(d1/
 
 public class Poly005 {
 	public static void main(String[] args) {
+		// 부모 = 자식 / 업캐스팅 / 타입캐스팅 필요X
 		Calc [] my = {new Plus(), new Minus(), new Mutiply(), new Divide()};
-		my[0].exec(10, 3);
-		my[1].exec(10, 3);
-		my[2].exec(10, 3);
-		my[3].exec(10, 3);
+		my[0].exec(10, 3); //더하기
+		my[1].exec(10, 3); //빼기
+		my[2].exec(10, 3); //곱하기
+		my[3].exec(10, 3); //나누기
 	}
 }
