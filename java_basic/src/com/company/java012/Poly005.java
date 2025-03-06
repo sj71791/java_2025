0cab9eb94c4243fb36363c53ae685621862b6985
sj <sj71791@naver.com>
2025-03-06 13:46:04 +0900

    2025-03-06

--- java_basic/src/com/company/java012/Poly005.java
+++ java_basic/src/com/company/java012/Poly005.java
@@ -23,19 +23,12 @@ class Divide extends Calc{void exec(double d1, double d2){System.out.println(d1/
 
 public class Poly005 {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		Calc [] my = {new Plus(), new Minus(), new Mutiply(), new Divide()};
-		my[0].exec(10, 3);
-		my[1].exec(10, 3);
-		my[2].exec(10, 3);
-		my[3].exec(10, 3);
-=======
+
 		// 부모 = 자식 / 업캐스팅 / 타입캐스팅 필요X
 		Calc [] my = {new Plus(), new Minus(), new Mutiply(), new Divide()};
 		my[0].exec(10, 3); //더하기
 		my[1].exec(10, 3); //빼기
 		my[2].exec(10, 3); //곱하기
 		my[3].exec(10, 3); //나누기
->>>>>>> 1aca09002bc068ce3c8b6b92998796fcd9ee515d
 	}
 }
